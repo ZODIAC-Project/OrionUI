8f7996947493cf77e597386f4e582b6da30de221
uZhW8Rgl <sonak53@googlemail.com>
2026-06-09 10:47:18 +0200

    fix: remove agent and UI v1

--- public/config.js
+++ public/config.js
@@ -2,9 +2,9 @@ window.__APP_CONFIG__ = {
   VITE_CHAT_API_BASE: '/api',
   VITE_CHAT_API_PATH: '/chat',
   VITE_AGENT_API_BASE: '/agent-api',
-  VITE_TOOL_USE_URL: '/api/tool-use',
+  VITE_TOOL_USE_URL: 'ws://130.149.158.32:30084/tool-use',
   VITE_BROKER_URL: 'ws://hivezodiac:8000/mqtt',
   VITE_MCP_CLIENT_URL: 'http://mcp-client-service:8000/health',
-  VITE_LLM_URL: 'http://elise.ise.tu-berlin.de:3000',
+  VITE_LLM_URL: 'http://elise.ise.tu-berlin.de:11434',
   VITE_GRAFANA_URL: 'http://130.149.158.131:30678/public-dashboards/645b6a3d77334d1da468182462276ded',
 };
